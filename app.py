078599040533c73d1e4933e2d7febae71415e6ab
Imaneelgh <imaneelghchach22@gmail.com>
2024-06-19 22:38:36 +0100

    Init commit

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from flask import Flask, render_template, Response
 import cv2
 import mediapipe as mp
 import numpy as np
-import math
+import os
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)
+# Déterminer si l'application est en cours d'exécution localement
+IS_LOCAL = os.environ.get('IS_LOCAL', 'true') == 'true'
+
+if IS_LOCAL:
+    camera = cv2.VideoCapture(0)
+else:
+    camera = None
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
-
 hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5, max_num_hands=1)
 
 class Button:
@@ -28,15 +33,15 @@ class Button:
         cv2.rectangle(img, (self.pos[0] + 5, self.pos[1] + 5),
                       (self.pos[0] + self.width + 5, self.pos[1] + self.height + 5),
                       shadow_color, -1)
-
-        cv2.rectangle(img, self.pos,
-                      (self.pos[0] + self.width, self.pos[1] + self.height),
+        
+        cv2.rectangle(img, self.pos, 
+                      (self.pos[0] + self.width, self.pos[1] + self.height), 
                       color, -1)
-
+        
         cv2.line(img, self.pos, (self.pos[0] + self.width, self.pos[1]), highlight_color, 2)
         cv2.line(img, self.pos, (self.pos[0], self.pos[1] + self.height), highlight_color, 2)
 
-        cv2.putText(img, self.value, (self.pos[0] + 20, self.pos[1] + 60),
+        cv2.putText(img, self.value, (self.pos[0] + 20, self.pos[1] + 60), 
                     cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
 
     def checkClick(self, x, y):
@@ -47,7 +52,7 @@ def findDistance(x, y, a, b):
 
 scientificButtonValues = [
     ["7", "8", "9", "/", "sin", "cos", "tan"],
-    ["4", "5",  "6", "*", "log", "ln", "sqrt"],
+    ["4", "5", "6", "*", "log", "ln", "sqrt"],
     ["1", "2", "3", "-", "(", ")", "^"],
     ["0", ".", "=", "+", "C", "<-", "pi"]
 ]
@@ -70,82 +75,85 @@ def gen_frames():
     global myEquation, delayCounter, buttonList
 
     while True:
-        success, frame = camera.read()
-        if not success:
-            break
+        if IS_LOCAL:
+            success, frame = camera.read()
+            if not success:
+                break
         else:
-            frame = cv2.flip(frame, 1)
-            imageHeight, imageWidth, _ = frame.shape
+            frame = np.zeros((480, 640, 3), dtype=np.uint8)  # placeholder frame
 
-            imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            results = hands.process(imgRGB)
+        frame = cv2.flip(frame, 1)
+        imageHeight, imageWidth, _ = frame.shape
 
-            display_area_color = (50, 50, 50)
-            cv2.rectangle(frame, (50, 50), (imageWidth - 50, 120), display_area_color, -1)
-            cv2.line(frame, (50, 50), (imageWidth - 50, 50), (255, 255, 255), 2)
-            cv2.line(frame, (50, 50), (50, 120), (255, 255, 255), 2)
-            cv2.line(frame, (50, 120), (imageWidth - 50, 120), (30, 30, 30), 2)
-            cv2.line(frame, (imageWidth - 50, 50), (imageWidth - 50, 120), (30, 30, 30), 2)
+        imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
 
-            for button in buttonList:
-                button.draw(frame)
+        display_area_color = (50, 50, 50)
+        cv2.rectangle(frame, (50, 50), (imageWidth - 50, 120), display_area_color, -1)
+        cv2.line(frame, (50, 50), (imageWidth - 50, 50), (255, 255, 255), 2)
+        cv2.line(frame, (50, 50), (50, 120), (255, 255, 255), 2)
+        cv2.line(frame, (50, 120), (imageWidth - 50, 120), (30, 30, 30), 2)
+        cv2.line(frame, (imageWidth - 50, 50), (imageWidth - 50, 120), (30, 30, 30), 2)
 
-            distance = float('inf')
-            index_x, index_y = None, None
-            middle_x, middle_y = None, None
+        for button in buttonList:
+            button.draw(frame)
 
-            if results.multi_hand_landmarks:
-                for handLandmarks in results.multi_hand_landmarks:
-                    for point in mp_hands.HandLandmark:
-                        normalizedLandmark = handLandmarks.landmark[point]
-                        pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(
-                            normalizedLandmark.x, normalizedLandmark.y, imageWidth, imageHeight
-                        )
+        distance = float('inf')
+        index_x, index_y = None, None
+        middle_x, middle_y = None, None
 
-                        if point == mp_hands.HandLandmark.INDEX_FINGER_TIP:
-                            if pixelCoordinatesLandmark is not None:
-                                index_x, index_y = pixelCoordinatesLandmark
+        if results.multi_hand_landmarks:
+            for handLandmarks in results.multi_hand_landmarks:
+                for point in mp_hands.HandLandmark:
+                    normalizedLandmark = handLandmarks.landmark[point]
+                    pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(
+                        normalizedLandmark.x, normalizedLandmark.y, imageWidth, imageHeight
+                    )
 
-                        if point == mp_hands.HandLandmark.MIDDLE_FINGER_TIP:
-                            if pixelCoordinatesLandmark is not None:
-                                middle_x, middle_y = pixelCoordinatesLandmark
-                            break
+                    if point == mp_hands.HandLandmark.INDEX_FINGER_TIP:
+                        if pixelCoordinatesLandmark is not None:
+                            index_x, index_y = pixelCoordinatesLandmark
 
-                if index_x is not None and middle_x is not None:
-                    frame = cv2.circle(frame, (index_x, index_y), radius=3, color=(0, 0, 255), thickness=5)
-                    distance = findDistance(index_x, index_y, middle_x, middle_y)
+                    if point == mp_hands.HandLandmark.MIDDLE_FINGER_TIP:
+                        if pixelCoordinatesLandmark is not None:
+                            middle_x, middle_y = pixelCoordinatesLandmark
+                        break
 
-                    if distance < 35:
-                        for button in buttonList:
-                            if button.checkClick(index_x, index_y) and delayCounter == 0:
-                                myValue = button.value
+            if index_x is not None and middle_x is not None:
+                frame = cv2.circle(frame, (index_x, index_y), radius=3, color=(0, 0, 255), thickness=5)
+                distance = findDistance(index_x, index_y, middle_x, middle_y)
 
-                                if myValue == "=":
-                                    try:
-                                        myEquation = str(eval(myEquation.replace('pi', str(np.pi)).replace('sqrt', 'np.sqrt').replace('log', 'np.log10').replace('ln', 'np.log').replace('^', '**').replace('sin', 'np.sin').replace('cos', 'np.cos').replace('tan', 'np.tan')))
-                                    except:
-                                        myEquation = "Error"
-                                elif myValue == "C":
-                                    myEquation = ""
-                                elif myValue == "<-":
-                                    myEquation = myEquation[:-1]
-                                else:
-                                    myEquation += myValue
+                if distance < 35:
+                    for button in buttonList:
+                        if button.checkClick(index_x, index_y) and delayCounter == 0:
+                            myValue = button.value
 
-                                delayCounter = 1
+                            if myValue == "=":
+                                try:
+                                    myEquation = str(eval(myEquation.replace('pi', str(np.pi)).replace('sqrt', 'np.sqrt').replace('log', 'np.log10').replace('ln', 'np.log').replace('^', '**').replace('sin', 'np.sin').replace('cos', 'np.cos').replace('tan', 'np.tan')))
+                                except:
+                                    myEquation = "Error"
+                            elif myValue == "C":
+                                myEquation = ""
+                            elif myValue == "<-":
+                                myEquation = myEquation[:-1]
+                            else:
+                                myEquation += myValue
 
-            if delayCounter != 0:
-                delayCounter += 1
-                if delayCounter > 10:
-                    delayCounter = 0
+                            delayCounter = 1
 
-            cv2.putText(frame, myEquation, (60, 110), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
+        if delayCounter != 0:
+            delayCounter += 1
+            if delayCounter > 10:
+                delayCounter = 0
 
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+        cv2.putText(frame, myEquation, (60, 110), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
 
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
@@ -155,11 +163,6 @@ def index():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-{
-  "rewrites": [{ "source": "/(.*)", "destination": "/app" }]
-}
-
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host='0.0.0.0', port=8000)
-
