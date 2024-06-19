85f9d28c2e43929a035d816233f7a532de9db038
Imaneelgh <imaneelghchach22@gmail.com>
2024-06-19 20:55:01 +0100

    is readyy !

--- app.py
+++ app.py
@@ -155,6 +155,11 @@ def index():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+{
+  "rewrites": [{ "source": "/(.*)", "destination": "/app" }]
+}
+
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host='0.0.0.0', port=8000)
+
